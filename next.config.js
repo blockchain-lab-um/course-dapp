86fc871a980f5d427d552efeefd5bebfb09e394b
andrazvrecko <andraz.vrecko@student.um.si>
2022-05-09 15:00:50 +0200

    added export script

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-const isProd = process.env.NODE_ENV === 'production';
+//const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = { nextConfig, assetPrefix: isProd ? 'course-dapp/' : '' };
-
+module.exports = { nextConfig };
