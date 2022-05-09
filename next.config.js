2254fd65dd98e74b18bd3480e3f8b9e04736f8bd
andrazvrecko <andraz.vrecko@student.um.si>
2022-05-09 15:29:00 +0200

    f

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-//const isProd = process.env.NODE_ENV === 'production';
+const debug = process.env.NODE_ENV === 'production';
 
-module.exports = { nextConfig };
+module.exports = {
+  nextConfig,
+  assetPrefix: !debug ? '/Next-gh-page-example/' : '',
+};
