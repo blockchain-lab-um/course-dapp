6885a73f9336846520ac519879854d36a8588808
andrazvrecko <andraz.vrecko@student.um.si>
2022-05-09 14:45:29 +0200

    ghp fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Solidity Course</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;600;700&display=swap"
           rel="stylesheet"
