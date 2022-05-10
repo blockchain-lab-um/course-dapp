c693a36bea432da4525eb117d476a6e03c060970
andyv09 <andyv09@users.noreply.github.com>
2022-05-10 12:30:59 +0000

    deploy: fca1d1653e46079162a1d25e5932c5ff68e71367

---
+++ _next/static/mLriY1_XddSQI-GjtaxHv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1f20afdec19d396e.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
