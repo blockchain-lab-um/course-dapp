f10864d55fe8453599e24452f06d5baac0bac6e8
andyv09 <andyv09@users.noreply.github.com>
2022-08-09 12:09:52 +0000

    deploy: b82e4f14ab830e5e624229b2eba8f3010c9b9146

---
+++ _next/static/9HSUtqjNuloHRNwlLaf8f/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ebd6b71a569f6f3e.js"],"/_error":["static/chunks/pages/_error-7dc6573fad4a39e8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
